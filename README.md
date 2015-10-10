86aba9ecde70af085dba64868ec1d0bfff607e18
alau99 <techawert@yahoo.com>
2015-10-10 13:02:38 -0700

    Update and finalize files

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
 # cookie-stand
 Cookie Stand App for Code Fellows
 
-Developed: Alexander Lau
+Developed: Alexander Lau, 10/10/2015
+
+====================================
+
+Update sales.html with an add location feature to form
+
+Alexander Lau, 10/9/2015
+
+====================================
+
+Update sales.html with a form and button
+
+Driver: John Sanford
+Navigator: Alexander Lau
+
+Alexander Lau, 10/8/2015
+
+====================================
+
+Create a new index.html per Pat's request
+
+Alexander Lau, 10/7/2015
+
+====================================
+
+Update app.js to implement a contructor function
+
+Alexander Lau, 10/6/2015
+
+====================================
+
+Create framework and app.js
+
+Alexander Lau, 10/5/2015
---
+++ app2.js
@@ -0,0 +1,54 @@
+function Cookies(location, minCustHour, maxCustHour, avgCookiesCust, cookiesHour, totalCookies, ul) {
+  this.location = location;
+  this.minCustHour = minCustHour;
+  this.maxCustHour = maxCustHour;
+  this.avgCookiesCust = avgCookiesCust;
+  this.cookiesHour = cookiesHour;
+  this.totalCookies = totalCookies;
+  this.ul = ul;
+
+  this.randCustHour = function(max, min) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  },
+  this.totalCookiesHour = function() {
+    return Math.floor(this.randCustHour(this.maxCustHour, this.minCustHour) * this.avgCookiesCust);
+  },
+  this.makeUL = function(time) {
+    for (var i = 0; i < time.length; i++) {
+      this.cookiesHour = this.totalCookiesHour();
+      this.totalCookies += this.cookiesHour;
+
+      var locationList = document.getElementById(this.ul);
+      var item = document.createElement('li');
+
+      item.appendChild(document.createTextNode(time[i] + this.cookiesHour + ' Cookies'));
+      locationList.appendChild(item);
+
+      if (i === 7) {
+        var item = document.createElement('li');
+
+        item.appendChild(document.createTextNode('Total: ' + this.totalCookies + ' Cookies'));
+        locationList.appendChild(item);
+      }
+    }
+  }
+}
+
+//====================================================================
+
+var time = ['10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: '];
+
+var pikePlace = new Cookies('Pike Place Market', 17, 88, 5.2, 0, 0, 'pike');
+pikePlace.makeUL(time);
+
+var seaTac = new Cookies('SeaTac Airport', 6, 44, 1.2, 0, 0, 'sea');
+seaTac.makeUL(time);
+
+var southCenter = new Cookies('Southcenter Mall', 11, 38, 1.9, 0, 0, 'mall');
+southCenter.makeUL(time);
+
+var bellSquare = new Cookies('Bellevue Square', 20, 48, 3.3, 0, 0, 'bell');
+bellSquare.makeUL(time);
+
+var alkiBeach = new Cookies('Alki', 3, 24, 2.6, 0, 0, 'alki');
+alkiBeach.makeUL(time);
